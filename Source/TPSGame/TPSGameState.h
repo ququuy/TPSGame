bf13e29c5ca78773f06232df44105db1c9dfc995
ququuy <=>
2021-12-18 18:36:02 +0800

    update

---
+++ Source/TPSGame/TPSGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "TPSGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPSGAME_API ATPSGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
